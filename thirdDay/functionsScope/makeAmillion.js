fc0e8366119e4ccfb4ae374d214d8d0071fc071e
obay <obaymardini@gmail.com>
2016-07-20 15:57:39 +0200

    solve the exercise in another way

--- thirdDay/functionsScope/makeAmillion.js
+++ thirdDay/functionsScope/makeAmillion.js
@@ -9,3 +9,19 @@ function makeAmillion(num) {
 
   return makeAmillion(num * 10);
 }
+
+// second way
+
+function makeAmillion(num) {
+  if ( num < 0 || isNaN(num)) {
+    return 'ERROR';
+  }
+
+  while ( num < 1000000) {
+    num *= 10;
+  }
+  if ( num > 1000000) {
+    return num;
+  }
+
+}
