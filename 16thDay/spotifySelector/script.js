2900baaf034b4fbda0428424e7d2eb964dcff196
obay <obaymardini@gmail.com>
2016-08-08 16:53:35 +0200

    use spotify API to look for songs by artist name or album name

---
+++ 16thDay/spotifySelector/script.js
@@ -0,0 +1,44 @@
+(function() {
+  var count = 0;
+  function createURL(name, type) {
+    return 'https://api.spotify.com/v1/search?q=' + encodeURIComponent(name) + '&type=' +type + '&offset=' + count + '&limit=5';
+  }
+
+  function loadSongs() {
+    console.log('hello')
+    var name = $('input').val();
+    var type = $('select').val();
+    console.log(type, name)
+    var url = createURL(name,type);
+    $.get(url,function(response){
+      if(type === 'album'){
+        for (var i =0; i < response.albums.items.length; i++) {
+          try {
+                $('<div><img src=' + response.albums.items[i].images[1].url + '><span>' + response.albums.items[i].name + '</span></div>').appendTo('#container');
+          } catch(e) {
+          }
+        }
+      } else {
+        for (var i =0; i < response.artists.items.length; i++) {
+          try {
+          $('<div><img src=' + response.artists.items[i].images[1].url + '><span>' + response.artists.items[i].name + '</span></div>').appendTo('#container');
+          } catch(e) {
+            console.log(e)
+          }
+        }
+      }
+
+      more = $('<button type="submit" id="more">more</button>').appendTo('#container')
+      $('#more').on('click', function() {
+        more.detach()
+        loadSongs();
+      });
+    })
+    count++;
+  }
+  $('#go').on('click', function() {
+    $('#container').empty();
+    loadSongs();
+  });
+
+})()
