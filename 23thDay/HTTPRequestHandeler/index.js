6ae303fb01a839d20ee424fde902179921c8108b
obay <obaymardini@gmail.com>
2016-08-17 18:14:30 +0200

    sole the whole challenge

--- 23thDay/HTTPRequestHandeler/index.js
+++ 23thDay/HTTPRequestHandeler/index.js
@@ -1,31 +1,51 @@
 var http = require('http');
 var myFirstServer = http.createServer(function(request, response) {
+
   var method = request.method;
   var url = request.url;
   var headers = request.headers;
   var body = [];
-  response.setHeader('Content-Type', 'application/json');
-  response.setHeader('X-Powered-By', 'bacon');
-  console.log('heelo')
+  console.log('method  ' + method + ' url ' + url + '   headers' + headers);
   request.on('error', function(err){
     console.log(err);
-  }).on('data', function(chunk) {
-    body.push(chunk)
-    //console.log(chunk);
-  }).on('end', function() {
-    body = Buffer.concat(body).toString();
-    console.log('boddy' + body);
-    response.statusCode = 200;
-    var responseBody = {
-      headers: headers,
-      method: method,
-      body: body,
-      url: url
-    };
-    response.end(JSON.stringify(responseBody));
   });
   response.on('error', function(err) {
     console.log('error in the response ' + err);
-  })
+  });
+  if(method !== 'GET' && method !== 'HEAD' && method !== 'PUT' && method !== 'POST') {
+    response.statusCode = 403;
+    response.end();
+  }
+  if(method === 'GET' || method === 'HEAD') {
+    response.setHeader('Content-Type', 'text/html');
+    response.statusCode = 200;
+  }
 
+  if(method === 'HEAD') {
+    response.end();
+  }else {
+
+    request.on('data', function(chunk) {
+      body.push(chunk)
+      //console.log(chunk);
+    }).on('end', function() {
+      body = Buffer.concat(body).toString();
+      if(method === 'PUT' || method === 'POST') {
+        console.log(body);
+        response.setHeader('location', '/');
+        response.statusCode = 303;
+        response.end();
+      } else {
+        response.statusCode = 200;
+        if(method === 'GET') {
+          response.end('<!doctype html><html><title>Hello World!</title></html>');
+        } else {
+            response.end('there is no body');
+        }
+      }
+    });
+    response.on('error', function(err) {
+      console.log('error in the response ' + err);
+    });
+  }
 }).listen(8080)
