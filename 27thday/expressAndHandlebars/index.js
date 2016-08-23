990f938ca5464fb25b72e62f2090cb40dd83939f
obay <obaymardini@gmail.com>
2016-08-23 16:46:29 +0200

    solve the second task of the exercise

--- 27thday/expressAndHandlebars/index.js
+++ 27thday/expressAndHandlebars/index.js
@@ -2,20 +2,21 @@ var express = require('express');
 var hb = require('express-handlebars');
 var app = express();
 var staticProjects = express.static(__dirname + '/projects');
-/*
-app.engine('handlebars', hb({defauleLayout: 'main'}));
 
-app.set('view engine', 'handlebars');
-app.get('/', function(req, res){
-  res.render('home');
-});
-*/
 app.use(staticProjects);
 app.engine('handlebars', hb());
 app.set('view engine', 'handlebars');
-app.get('/dono',function(req,res){
-  res.render('hello',
-      {obj:[{link:'/hangman', name: 'hangman'},{link:'/ticker', name: 'ticker'},{link:'/spotifySelector', name: 'Spotify Selector'},{link:'/wk2_carousel', name: 'wk2_carousel'}]}
-  );
+app.get('/', function(req, res){
+  //the body
+  res.render('body',{
+    layout: 'main',
+    myList: [{link:'/hangman/description', name: 'hangman'},{link:'/ticker', name: 'ticker'},{link:'/spotifySelector', name: 'Spotify Selector'},{link:'/wk2_carousel', name: 'wk2_carousel'}]
+  })
+})
+app.get('/:project/description', function(req,res){
+    res.render('description',{
+      layout: 'main',
+      details: {name: req.params.project, description: req.params.project + ' is super fun', link: '/' + req.params.project}
+    });
 });
 app.listen(8080);
